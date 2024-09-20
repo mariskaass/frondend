c1ac4adc1299e865a60486c5d8acd840c31b1e2e
mariskaass <mariska.boelhouwers@hva.nl>
2024-09-20 13:54:06 +0200

    wfegrbdgf

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,8 @@ console.log("hi");
 const articles = document.getElementsByClassName('snap_article');
 const section_snap = document.getElementById('snap');
 const header = document.querySelector('header');
-const logo = document.getElementById('logo')
+const logo = document.getElementById('logo');
+const cursor = document.getElementsByTagName('div')[0];
 //VARIABELEN
 
 
@@ -15,7 +16,6 @@ const logo = document.getElementById('logo')
 function scrollsnap() {
     console.log(window.scrollY)
     if (window.scrollY > (window.innerHeight)*0.01) {
-        console.log("we zijn er");
         for(let i = 0; i < articles.length; i++) {
             articles[i].classList.remove('snap_child');
         }
@@ -37,12 +37,21 @@ function header_change() {
         header.classList.remove('header2')
         logo.src = "/images/RR.svg"
     }
-}   
+}  
+
+function cursor_move(event) {
+    cursor.style.top = event.clientY + 'px'
+    cursor.style.left = event.clientX + 'px'
+}
 
 
 
 
 //EVENTLISTENERS
+window.addEventListener('mousemove',cursor_move);
+// window.addEventListener('scroll',scrollsnap);
+// window.addEventListener('scroll',header_change);
+// window.addEventListener('scroll',()=>{console.log('scroll')});
 
 //SETINTERVAL
 setInterval(scrollsnap,100);
